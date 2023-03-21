1aebb608bdfb124f65852134125e6c024ae69817
Nishant Rodrigues <nishantjr@gmail.com>
2023-03-21 16:32:57 -0500

    Move tests to pytest

--- test.py
+++ test.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 
+import pytest
 from collections import defaultdict
 from glob import glob
 from os import path, makedirs
 import time
 from tabulate import tabulate
-from typing import Dict, List, NamedTuple, Optional, Tuple
+from typing import Dict, List, NamedTuple, Optional, Tuple, no_type_check
 from subprocess import check_call, check_output
 from sys import argv
 
@@ -40,12 +41,18 @@ class _Benchmark():
         return self
     def __exit__(self, exc_type, exc_val, exc_tb):
         end = time.time_ns()
-        runtime = (end - self.start) // (1000 * 1000)
+        runtime = (end - self.start)
         benchmarks[self.test_name] = benchmarks[self.test_name]._replace(**{self.aspect: runtime})
 
 def benchmark(test_name: str, aspect: str) -> _Benchmark:
     return _Benchmark(test_name, aspect)
 
+
+### PyTest configuration & Helpers ##########
+
+@no_type_check
+def slow(*args):
+    return pytest.param(*args, marks=pytest.mark.slow)
 
 ### Helpers #######################
 
@@ -61,8 +68,30 @@ def run_proof_gen(mode: str, theorem: str, regex: str, output_file: str) -> None
     with open(output_file, 'w') as f:
         check_call(['./proof-gen.py', mode, theorem, regex], stdout=f)
 
-### Tests #########################
 
+### Tests: Maude #######################
+
+def test_maude_version() -> None:
+    maude.check_maude_version()
+
+def test_maude_unit_tests() -> None:
+    assert maude.reduce_in_module('test.maude', 'TEST', 'TestResult', 'unit-tests') == 'passed'
+
+
+### Test: proof check base MM0/1 files ##########################
+
+makedirs(test_dir, exist_ok=True)
+last_mm0_file = None
+
+base_mm_tests = []
+for f in sorted((glob('*.mm0') + glob('*.mm1'))):
+    if path.splitext(f)[1] == '.mm0':
+        last_mm0_file = path.join(test_dir, 'joined.' + f)
+        join(f, last_mm0_file)
+    assert last_mm0_file
+    base_mm_tests += [(last_mm0_file, f)]
+
+@pytest.mark.parametrize('mm0_file,mm1_file', base_mm_tests)
 def test_mm(mm0_file: str, mm1_file: str) -> None:
     basename = path.basename(mm1_file)
     test_name, extension = path.splitext(basename)
@@ -78,6 +107,25 @@ def test_mm(mm0_file: str, mm1_file: str) -> None:
     with benchmark(test_name, 'check'):     check(output_mmb, mm0_file)
 
 
+### Test: proof generated certificates ##########################
+
+@pytest.mark.parametrize('theorem,test_name,regex',
+[   ('fp-implies-regex-pub', 'matches-self-a',             'a ->> a'),
+    ('fp-implies-regex-pub', 'matches-self-aa.aa',         '(a . a) . (a . a) ->> (a . a) . (a . a)'),
+    ('fp-implies-regex-pub', 'matches-self-a+b',           '(a + b) ->> (a + b)'),
+    ('fp-implies-regex-pub', 'matches-self-bbx.bx',        '(( (b . b) * ) . ( b * )) ->> (( (b . b) * ) . ( b * ))'),
+    ('top-implies-fp-pub',   'example-in-paper-1',         '(a . a)* ->> (((a *) . a) + epsilon) '),
+    ('main-goal',            'a-or-b-star',                '(a + b)*'),
+    ('top-implies-fp-pub',   'kleene-star-star-1',         '(a *) * ->> (a *)'),
+    ('fp-implies-regex-pub', 'kleene-star-star',           '(a *) * ->> (a *)'),
+    ('fp-implies-regex-pub', 'example-in-paper',           '(a . a)* ->> (((a *) . a) + epsilon) '),
+    ('top-implies-fp-pub',   'alternate-top-1',            '((a *) . b) * + (((b *) . a) *)'),
+    ('fp-implies-regex-pub', 'alternate-top',              '((a *) . b) * + (((b *) . a) *)'),
+    ('top-implies-fp-pub',   'even-or-odd-1',              '((((a . a) + (a . b)) + (b . a)) + (b . b)) * + ((a + b) . (((((a . a) + (a . b)) + (b . a)) + (b . b)) *))'),
+    ('fp-implies-regex-pub', 'even-or-odd',                '((((a . a) + (a . b)) + (b . a)) + (b . b)) * + ((a + b) . (((((a . a) + (a . b)) + (b . a)) + (b . b)) *))'),
+    ('top-implies-fp-pub',   'no-contains-a-or-no-only-b-1', '(~ (top . (a . top))) + ~ (b *)'),
+    ('fp-implies-regex-pub', 'no-contains-a-or-no-only-b', '(~ (top . (a . top))) + ~ (b *)'),
+])
 def test_regex(theorem: str, test_name: str, regex: str) -> None:
     output_mm0_file = path.join(test_dir, test_name + '.mm0')
     output_joined_mm0_file = path.join(test_dir, test_name + '.joined.mm0')
@@ -89,25 +137,39 @@ def test_regex(theorem: str, test_name: str, regex: str) -> None:
     with benchmark(test_name, 'gen_mm1'): run_proof_gen('mm1', theorem, regex, output_mm1_file)
     test_mm(output_joined_mm0_file, output_mm1_file)
 
+# Benchmarks from Unified Decision Procedures for Regular Expression Equivalence
+# https://citeseerx.ist.psu.edu/document?repid=rep1&type=pdf&doi=f650281fc011a2c132690903eb443ff1ab3298f7
 
-### Parametric tests #################
+@pytest.mark.parametrize('n', [1, 2, 4, slow(10), slow(20), slow(30), slow(40), slow(100)])
+def test_regex_match_l(n: int) -> None:
+    test_regex('fp-implies-regex-pub',  'match-l-{:03d}-1'.format(n), 'match-l({})'.format(n))
+    test_regex('top-implies-fp-pub',    'match-l-{:03d}-2'.format(n), 'match-l({})'.format(n))
 
-FAST = False
-SLOW = True
-TestData = Tuple[bool, str, str, str]
+@pytest.mark.parametrize('n', [1, 2, 4, slow(10), slow(20), slow(30)])
+def test_regex_match_r(n: int) -> None:
+    test_regex('fp-implies-regex-pub',  'match-r-{:03d}-1'.format(n), 'match-r({})'.format(n))
+    test_regex('top-implies-fp-pub',    'match-r-{:03d}-2'.format(n), 'match-r({})'.format(n))
 
-def param_test(theorem: str, test_name: str, regex: str, fast: List[int] = [], slow: List[int] = []) -> List[TestData]:
-    ret : List[TestData] = []
-    for param in fast:
-        ret.append((FAST, theorem, test_name.format(param), regex.format(param)))
-    for param in slow:
-        ret.append((SLOW, theorem, test_name.format(param), regex.format(param)))
-    return ret
+@pytest.mark.parametrize('n', [1, 2, 4, slow(10), slow(20), slow(30)])
+def test_regex_eq_l(n: int) -> None:
+    test_regex('fp-implies-regex-pub',  'eq-l-{:03d}-1'.format(n), 'eq-l({})'.format(n))
+    test_regex('top-implies-fp-pub',    'eq-l-{:03d}-2'.format(n), 'eq-l({})'.format(n))
+
+@pytest.mark.parametrize('n', [1, 2, 4, slow(10), slow(20), slow(30)])
+def test_regex_eq_r(n: int) -> None:
+    test_regex('fp-implies-regex-pub',  'eq-r-{:03d}-1'.format(n), 'eq-r({})'.format(n))
+    test_regex('top-implies-fp-pub',    'eq-r-{:03d}-2'.format(n), 'eq-r({})'.format(n))
+
+@pytest.mark.parametrize('n', [1, 2, 4, slow(10), slow(20), slow(30)])
+def test_regex_eq_lr(n: int) -> None:
+    test_regex('fp-implies-regex-pub',  'eq-lr-{:03d}-1'.format(n), 'eq-lr({})'.format(n))
+    test_regex('top-implies-fp-pub',    'eq-lr-{:03d}-2'.format(n), 'eq-lr({})'.format(n))
 
 
 ### Randomized tests using hypothesis
 
 from typing import Callable
+import hypothesis
 from hypothesis import given, settings
 from hypothesis.strategies import composite, just, recursive, SearchStrategy, DrawFn
 
@@ -136,79 +198,11 @@ def regex() -> SearchStrategy[str]:
                      lambda sub: concat(sub) | kleene(sub) | plus(sub))
 
 @given(regex())
-@settings(deadline=None)
+@settings(deadline=None, verbosity=hypothesis.Verbosity.verbose)
+@pytest.mark.slow
 def test_equiv(regex):
     print(regex)
     test_regex('fp-implies-regex-pub', 'rand-1', regex + ' ->> ' + regex)
 
 
-### Main #######################
-
-def test_maude_unit_tests() -> None:
-    assert maude.reduce_in_module('test.maude', 'TEST', 'TestResult', 'unit-tests') == 'passed'
-
-maude.check_maude_version()
-test_maude_unit_tests()
-
-# Arg parsing
-usage = 'usage: test [--no-slow-tests]'
-run_slow_tests = True
-assert len(argv) <= 2, usage
-if len(argv) == 2:
-    if argv[1] == '--no-slow-tests':
-        run_slow_tests = False
-    else:
-        raise AssertionError(usage)
-
-# Check concrete MM0/MM1 lemmas
-makedirs(test_dir, exist_ok=True)
-last_mm0_file = None
-for f in sorted((glob('*.mm0') + glob('*.mm1'))):
-    if path.splitext(f)[1] == '.mm0':
-        last_mm0_file = path.join(test_dir, 'joined.' + f)
-        join(f, last_mm0_file)
-    assert last_mm0_file
-    test_mm(last_mm0_file, f)
-
-# Regular expression tests
-tests : List[TestData] = [
-    (FAST, 'fp-implies-regex-pub', 'matches-self-a',             'a ->> a'),
-    (FAST, 'fp-implies-regex-pub', 'matches-self-aa.aa',         '(a . a) . (a . a) ->> (a . a) . (a . a)'),
-    (FAST, 'fp-implies-regex-pub', 'matches-self-a+b',           '(a + b) ->> (a + b)'),
-    (FAST, 'fp-implies-regex-pub', 'matches-self-bbx.bx',        '(( (b . b) * ) . ( b * )) ->> (( (b . b) * ) . ( b * ))'),
-    (FAST, 'top-implies-fp-pub',   'example-in-paper-1',         '(a . a)* ->> (((a *) . a) + epsilon) '),
-    (FAST, 'main-goal',            'a-or-b-star',                '(a + b)*'),
-    (FAST, 'top-implies-fp-pub',   'kleene-star-star-1',         '(a *) * ->> (a *)'),
-    (FAST, 'fp-implies-regex-pub', 'kleene-star-star',           '(a *) * ->> (a *)'),
-    (FAST, 'fp-implies-regex-pub', 'example-in-paper',           '(a . a)* ->> (((a *) . a) + epsilon) '),
-    (FAST, 'top-implies-fp-pub',   'alternate-top-1',            '((a *) . b) * + (((b *) . a) *)'),
-    (FAST, 'fp-implies-regex-pub', 'alternate-top',              '((a *) . b) * + (((b *) . a) *)'),
-    (FAST, 'top-implies-fp-pub',   'even-or-odd-1',              '((((a . a) + (a . b)) + (b . a)) + (b . b)) * + ((a + b) . (((((a . a) + (a . b)) + (b . a)) + (b . b)) *))'),
-    (FAST, 'fp-implies-regex-pub', 'even-or-odd',                '((((a . a) + (a . b)) + (b . a)) + (b . b)) * + ((a + b) . (((((a . a) + (a . b)) + (b . a)) + (b . b)) *))'),
-    (FAST, 'top-implies-fp-pub',   'no-contains-a-or-no-only-b-1', '(~ (top . (a . top))) + ~ (b *)'),
-    (FAST, 'fp-implies-regex-pub', 'no-contains-a-or-no-only-b', '(~ (top . (a . top))) + ~ (b *)'),
-
-    # Benchmarks from Unified Decision Procedures for Regular Expression Equivalence
-    # https://citeseerx.ist.psu.edu/document?repid=rep1&type=pdf&doi=f650281fc011a2c132690903eb443ff1ab3298f7
-    *param_test('fp-implies-regex-pub', 'match-l-{:03d}', 'match-l({})', fast=[1,4], slow=[2, 10, 20, 30, 40, 100]),
-    *param_test('fp-implies-regex-pub', 'match-r-{:03d}', 'match-r({})', fast=[1,4], slow=[2, 10, 20, 30]),
-    *param_test('fp-implies-regex-pub',    'eq-l-{:03d}',    'eq-l({})', fast=[1,4], slow=[2, 10, 20, 30]),
-    *param_test('fp-implies-regex-pub',    'eq-r-{:03d}',    'eq-r({})', fast=[1,4], slow=[2, 10, 20, 30]),
-    *param_test('fp-implies-regex-pub',   'eq-lr-{:03d}',   'eq-lr({})', fast=[1,4], slow=[2, 10, 20, 30]),
-
-    *param_test('top-implies-fp-pub', 'match-l-{:03d}-1', 'match-l({})', fast=[1,4], slow=[2, 10, 20, 30, 40, 100]),
-    *param_test('top-implies-fp-pub', 'match-r-{:03d}-1', 'match-r({})', fast=[1,4], slow=[2, 10, 20, 30]),
-    *param_test('top-implies-fp-pub',    'eq-l-{:03d}-1',    'eq-l({})', fast=[1,4], slow=[2, 10, 20, 30]),
-    *param_test('top-implies-fp-pub',    'eq-r-{:03d}-1',    'eq-r({})', fast=[1,4], slow=[2, 10, 20, 30]),
-    *param_test('top-implies-fp-pub',   'eq-lr-{:03d}-1',   'eq-lr({})', fast=[1,4], slow=[2, 10, 20, 30]),
-]
-
-for test in tests:
-    is_slow, theorem, name, expression = test
-    if (not is_slow) or run_slow_tests:
-        test_regex(theorem, name, expression)
-
-test_equiv()
-
-print()
 print_benchmarks()
