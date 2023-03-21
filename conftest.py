f6ef8b26a214794491b9d4c9544ed0d33efed3a5
Nishant Rodrigues <nishantjr@gmail.com>
2023-03-21 16:32:57 -0500

    Move tests to pytest

---
+++ conftest.py
@@ -0,0 +1,15 @@
+import pytest
+from typing import no_type_check
+
+@no_type_check
+def pytest_addoption(parser):
+    parser.addoption('--skip-slow', action='store_true', help='Skip tests marked slow')
+
+@no_type_check
+def pytest_runtest_setup(item):
+    if 'slow' in item.keywords and item.config.getoption("--skip-slow"):
+        pytest.skip("slow test.")
+
+@no_type_check
+def pytest_configure(config):
+    config.addinivalue_line("markers", "slow: mark test as slow.")
