e7464e9262ccecd4beb890bcd2f4d445f48b40d7
Nishant Rodrigues <nishantjr@gmail.com>
2023-03-21 15:01:05 -0500

    mypy: Also type-check proof-gen.py

--- proof-gen.py
+++ proof-gen.py
@@ -2,19 +2,19 @@
 
 import tempfile, shutil, os, sys, subprocess, re
 
-def check_maude_version():
+def check_maude_version() -> None:
     from packaging import version
     expected_version = version.parse('3.2.1')
     actual_version = version.parse(subprocess.check_output(['maude', '--version'], text=True))
     assert actual_version >=  expected_version, "Expected Maude version '{}' or greater in PATH, got '{}'".format(expected_version, actual_version)
 
-def reduce_in_module(src, module, expected_sort, term):
-    output = subprocess.check_output(
+def reduce_in_module(src: str, module: str, expected_sort: str, term: str) -> str:
+    output_str = subprocess.check_output(
         ['maude', '-no-banner', '-no-wrap', '-batch', src],
         input='reduce in {0} : {1} . \n'.format(module, term),
         text=True
     )
-    output = output.split('\n')
+    output = output_str.split('\n')
 
     # Sanity check
     assert(output[0] == '=========================================='), output
@@ -29,7 +29,7 @@ def reduce_in_module(src, module, expected_sort, term):
     output[0] = output[0][len(result_string):]
     return '\n'.join(output)
 
-def cleanup_maude_output(s):
+def cleanup_maude_output(s: str) -> str:
     s = s.replace("'", "")
     s = s.replace("-", "_")
     s = s.replace("[", "(")
