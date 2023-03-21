1d33ef7fdf9a3c5d766fdf5c712ecf949df60bf2
Nishant Rodrigues <nishantjr@gmail.com>
2023-03-21 12:47:38 -0500

    Additional tests that were helpful during debugging

--- test.py
+++ test.py
@@ -121,8 +121,10 @@ for f in sorted((glob('*.mm0') + glob('*.mm1'))):
 
 # Regular expression tests
 tests : List[TestData] = [
-    (fast, 'fp-implies-regex-pub', 'a+b-implies-a+b',            '(a + b) ->> (a + b)'),
-    (fast, 'fp-implies-regex-pub', 'eq-bbx.bx',                  '(( (b . b) * ) . ( b * )) ->> (( (b . b) * ) . ( b * ))'),
+    (fast, 'fp-implies-regex-pub', 'matches-self-a',             'a ->> a'),
+    (fast, 'fp-implies-regex-pub', 'matches-self-aa.aa',         '(a . a) . (a . a) ->> (a . a) . (a . a)'),
+    (fast, 'fp-implies-regex-pub', 'matches-self-a+b',           '(a + b) ->> (a + b)'),
+    (fast, 'fp-implies-regex-pub', 'matches-self-bbx.bx',        '(( (b . b) * ) . ( b * )) ->> (( (b . b) * ) . ( b * ))'),
     (fast, 'top-implies-fp-pub',   'example-in-paper-1',         '(a . a)* ->> (((a *) . a) + epsilon) '),
     (fast, 'main-goal',            'a-or-b-star',                '(a + b)*'),
     (fast, 'top-implies-fp-pub',   'kleene-star-star-1',         '(a *) * ->> (a *)'),
