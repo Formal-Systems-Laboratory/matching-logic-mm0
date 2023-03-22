7a7447d996276ded6c44307fe87f3657563cbc63
Nishant Rodrigues <nishantjr@gmail.com>
2023-03-22 12:10:33 -0500

    maude.py: Die when warnings are produced

--- maude.py
+++ maude.py
@@ -7,23 +7,29 @@ def check_maude_version() -> None:
     assert actual_version >=  expected_version, "Expected Maude version '{}' or greater in PATH, got '{}'".format(expected_version, actual_version)
 
 def reduce_in_module(src: str, module: str, expected_sort: str, term: str) -> str:
-    output_str = subprocess.check_output(
-        ['maude', '-no-banner', '-no-wrap', '-batch', src],
-        input='reduce in {0} : {1} . \n'.format(module, term),
-        text=True
-    )
+    command = ['maude', '-no-banner', '-no-wrap', '-batch', src]
+    input = 'reduce in {0} : {1} . \n'.format(module, term)
+    process = subprocess.Popen(command, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    output_str, err_output_str = process.communicate(input=input)
+    if process.returncode != 0:
+        err_msg = "Maude exited with code: {} \n\nstderr:\n===========\n{}\n\nstdout\n===========\n{}\n".format(process.returncode, output_str, err_output_str)
+        raise Exception(err_msg)
+    elif len(err_output_str):
+        err_msg = "Maude produced warnings: {} \n\nstderr:\n===========\n{}\n\nstdout\n===========\n{}\n".format(process.returncode, output_str, err_output_str)
+        raise Exception(err_msg)
+
     output = output_str.split('\n')
 
     # Sanity check
     assert(output[0] == '=========================================='), output
-    assert(output[1].startswith('reduce in {0}'.format(module)))
-    assert(output[2].startswith('rewrites: '))
-    assert(output[-2:] == ['Bye.', ''])
-    output = output[3:-2]
+    assert(output[1].startswith('reduce in {0}'.format(module))), output
+    assert(output[2].startswith('rewrites: ')), output
+    assert(output[-2:] == ['Bye.', '']), output
 
+    # Parsing
+    output = output[3:-2]
     result_string = 'result {0}: '.format(expected_sort)
     assert(output[0].startswith(result_string)), output[0]
-
     output[0] = output[0][len(result_string):]
     return '\n'.join(output)
 
