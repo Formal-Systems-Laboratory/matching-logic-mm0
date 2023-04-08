ceac20ce3f3f8606d806b39c3cdd6d7ae967287b
Nishant Rodrigues <nishantjr@gmail.com>
2023-04-08 13:21:24 -0500

    Temporarily stop random test generation

--- test.py
+++ test.py
@@ -172,9 +172,8 @@ def regex() -> SearchStrategy[str]:
     return recursive(bot() | epsilon() | letters(),
                      lambda sub: concat(sub) | kleene(sub) | plus(sub))
 
-@given(regex())
-@settings(deadline=20*1000, verbosity=hypothesis.Verbosity.verbose, max_examples=10)
-@pytest.mark.slow
-def test_equiv(exp):
-    test_regex_implies_self(exp)
-    sys.stdout.write('I can print clearly now.')
+# @given(regex())
+# @settings(deadline=20*1000, verbosity=hypothesis.Verbosity.verbose, max_examples=10)
+# @pytest.mark.slow
+# def test_equiv(exp):
+#     test_regex_implies_self(exp)
